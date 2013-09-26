dff4f7f4a001e4da8282b6fea7fc48229a08f44a
boo1ean <boo1ean0807@gmail.com>
2013-09-26 23:22:31 +0300

    Implement perception app class, add socketio server

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
 var l = function() {
-    console.log.apply(console, arguments);
+	console.log.apply(console, arguments);
 }
 
-var net = require('net');
+var Perception = require('./lib/perception');
+var app = new Perception();
 
-var gyro = 71;
-var acc  = 65;
-
-var server = net.createServer(function(c) {
-  l("Connection opened");
-
-  c.on('data', function(data) {
-      var time = data.readDoubleLE(0);
-      var type = data.readUInt8(32);
-
-      var x = data.readDoubleLE(8);
-      var y = data.readDoubleLE(16);
-      var z = data.readDoubleLE(24);
-
-      l(x.toFixed(10), y.toFixed(10), z.toFixed(10));
-  });
-
-  c.on('end', function() {
-    l('Connection closed');
-  });
-
-  c.pipe(c);
-});
-
-server.listen(3000, function() {
-    l('Server is listening port 3000');
-});
+app.gyroscope(function() {
+	l('gyroscope');
+}).accelerometer(function() {
+	l('accelerometer');
+}).perceive();
