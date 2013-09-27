d0ee8e0781c161ba4976cd34fd9e160049a5e2e6
boo1ean <boo1ean0807@gmail.com>
2013-09-27 15:56:01 +0300

    Show full device stats table + gyroscope graph

--- app/app.js
+++ app/app.js
@@ -14,7 +14,7 @@ define(function(require) {
 		},
 
 		setupEvents: function() {
-			this.socket = io.connect('http://localhost:3001');
+			this.socket = io.connect('http://192.168.1.31:3001');
 
 			var g = graph.create();
 
@@ -23,12 +23,34 @@ define(function(require) {
 			var z = g('blue');
 
 			var vel = graph.create();
-			var v = vel('black');
+			var v = vel('cyan');
+
+			var dist = graph.create();
+			var d = dist('magenta');
+
+			var gyro = graph.create();
+			var gx = gyro('red');
+			var gy = gyro('green');
+			var gz = gyro('blue');
 
 			var prev     = 0;
 			var delta    = 0;
 			var velocity = 0;
+			var distance = 0;
 			var boost    = 0;
+
+			var ui = {
+				ax: document.getElementById('ax'),
+				ay: document.getElementById('ay'),
+				az: document.getElementById('az'),
+				gx: document.getElementById('gx'),
+				gy: document.getElementById('gy'),
+				gz: document.getElementById('gz'),
+				v: document.getElementById('v'),
+				d: document.getElementById('d')
+			}
+
+			var fixed = 12;
 
 			this.socket.on('a', function(data) {
 				delta = data.time - prev;
@@ -37,14 +59,38 @@ define(function(require) {
 
 				velocity += boost;
 
+				if (Math.abs(data.x) < 0.03) {
+					velocity = 0;
+				}
+
+				diff = delta * velocity;
+				distance += diff;
+
 				x(data.x);
 				y(data.y);
 				z(data.z);
-                v(velocity);
+				v(velocity);
+				d(distance / 5);
+
+				ui.ax.innerHTML = (data.x).toFixed(fixed);
+				ui.ay.innerHTML = (data.y).toFixed(fixed);
+				ui.az.innerHTML = (data.z).toFixed(fixed);
+				ui.v.innerHTML  = (velocity).toFixed(fixed);
+				ui.d.innerHTML  = (distance / 5).toFixed(fixed);
+			});
+
+			this.socket.on('g', function(data) {
+				gx(data.x);
+				gy(data.y);
+				gz(data.z);
+				ui.gx.innerHTML = data.x.toFixed(fixed);
+				ui.gy.innerHTML = data.y.toFixed(fixed);
+				ui.gz.innerHTML = data.z.toFixed(fixed);
 			});
 
 			document.getElementById('reset').onclick = function() {
 				velocity = 0;
+				distance = 0;
 			};
 		},
 
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var Perception = require('./lib/perception');
 var app = new Perception();
 
 var handleGyroscope = function(time, x, y, z) {
-	//this.emit('g', {x: x, y: y, z: z});
+    this.emit('g', {x: x, y: y, z: z});
 }
 
 var handleAccelerometer = function(time, x, y, z) {
